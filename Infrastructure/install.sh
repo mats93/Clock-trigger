1742a20e4433249c1e4d011784eaa66b6a49ebe2
mats <moskjaer@stud.ntnu.no>
2018-10-27 19:47:21 +0200

    Added HEAT templates and boot-script to download and run the app.

---
+++ Infrastructure/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Installation script for the Clock trigger application.
+
+# Download the application.
+wget https://github.com/mats93/Clock-trigger/releases/download/v1.0.0/ClockTrigger
+
+# Make it executable.
+chmod +x ClockTrigger
+
+# Run the application.
+./ClockTrigger
